863d53616cef6daf72967cdee2df005a821b8801
Ananya <ananya.banerjee.rr@gmail.com>
2020-08-23 18:08:41 -0500

    Updated code!

--- mymotivationalapp.py
+++ mymotivationalapp.py
@@ -30,7 +30,7 @@ DATA_URL_TRAIN= (
 #load the individual vectorizer from disk
 #vectorizer=joblib.load("Vectorizers/vectorizer_toxic.sav")
 
-st.title("Welcome to your daily dose of motivation!")
+st.title("Welcome to your daily dose of motivation!😊")
 st.markdown("This application is a Streamlit dashboard that can be used to display a motivational quote!")
 #st.markdown("### My first streammlit dashboard")
 
@@ -40,9 +40,9 @@ st.sidebar.info("This is a demo application written for exploring motivational q
 
 #display image
 from PIL import Image
-im = Image.open('m.jpeg')
-#image=im.resize((int(im.size[0]/3),int(im.size[1]/3)), 0)
-image=im
+im = Image.open('m5.jpg')
+image=im.resize((int(im.size[0]/2),int(im.size[1]/2)), 0)
+#image=im
 st.image(image) #use_column_width=True)
 
 
@@ -58,7 +58,7 @@ def load_data(nrows, col):
     #data.drop_duplicates(inplace=True, keep = False)
     data=data.drop_duplicates(subset=['Quote'], keep=False)
 
-    st.write("columns", data.columns)
+    #st.write("columns", data.columns)
 
     #drop nan values
     data.dropna(subset=[col], inplace=True)
@@ -81,21 +81,21 @@ original_data= data
 
 
 #enter comment_text
-input=st.text_input(label='Enter text')
+#input=st.text_input(label='Enter text')
 #display it
-st.write(input)
+#st.write(input)
 
 
 #another ## QUESTION:
-st.header("View training data according to category")
+st.header("How many motivational quotes would you like to view?")
 #
 #st.markdown("")
 num_datapoints = st.selectbox(
-    'How many data points would you like to view?',[0, 5, 10, 50, 100, 500, 1000]
+    'How many data points would you like to view?',[1, 5, 10, 50, 100, 500, 1000]
      )
 
 
-st.header("Five Motivational Thoughts!")
+st.header("Your customized motivational thoughts!")
 #sample 5 motivational comments
 cl = data['Quote'].sample(num_datapoints).values
 #a=list(cl['comment_text'])
@@ -103,12 +103,13 @@ cl = data['Quote'].sample(num_datapoints).values
 st.table(cl)
 
 #Ques
-st.header("How many categories are there?")
-
-st.write(np.unique(data['Category']))
+st.header("The motivational thoughts belong to these categories!")
+v=list(np.unique(data['Category']))
+v.remove("")
+st.write(v)
 
 #another chart: # Pie chart
-st.header("Pie chart showing percentage of each toxicity type")
+st.header("Pie chart showing percentage of each category of thoughts in our database!")
 #find counts
 counts=list(np.unique(data['Category'].value_counts()))
 labels=list(np.unique(data['Category']))
@@ -119,19 +120,26 @@ st.plotly_chart(fig)
 
 #another questions
 ## QUESTION:
-st.header("How many datapoints exist for each category")
+st.header("Datapoints existing in our databse belonging to aforementioned categories!")
 #slider for asking type of Toxicity
 st.write(data['Category'].value_counts())
 
 
-#another # QUESTION:
-st.header("Choice of graph as per category!")
 #
-column = st.selectbox('Categories', list(np.unique(data['Category'])))
+st.header("Would you like to choose the category to which your thought of the data should belong to?")
+
+ans=st.selectbox("Choice", ['Yes','No'])
+
+if ans=='Yes':
+   d=st.selectbox("Which category would you like to see?", labels)
+
+   d1=list(data[data['Category']==d]['Quote'].sample(1))[0]
+   st.write("Your motivational quote: ")
+   st.markdown(d1)
+else:
+    st.markdown("Your wish is my command! 😊")
 
 
-
-st.area_chart(data.loc[data['Category']==column, 'Quote'].head(100))
 
 
 
@@ -142,13 +150,17 @@ if st.checkbox("Show Raw Data", False):
     st.write(data.head(100))
 
     #heading
-    st.header("Graph showing training data!")
+    #st.header("Graph showing training data!")
     #make a chart_data
     #st.line_chart(data[['toxic','severe_toxic','insult','obscene','identity_hate','threat']].head(10000))
     #st.bar_chart(data[['toxic','severe_toxic','insult','obscene','identity_hate','threat']].head(10000))
     #st.line_chart(data['Category'].head(10000))
     #st.area_chart(data['Category'].head(10000))
     #st.area_chart(data.head(10000))
-    c = alt.Chart(data).mark_circle().encode(x='Quote', y='Category')#, size='c',
+    #c = alt.Chart(data).mark_circle().encode(x='Quote', y='Category')#, size='c',
                                        #color='c')
-    st.altair_chart(c, width=-1)
+    #st.altair_chart(c, width=-1)
+    #another # QUESTION:
+    st.header("Choice of graph as per category!")
+    column = st.selectbox('Categories', list(np.unique(data['Category'])))
+    st.area_chart(data.loc[data['Category']==column, 'Quote'].head(100))
